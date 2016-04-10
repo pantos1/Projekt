496b912bbfd5161b202709a921dfe64c437aac74
Kubaaj <jakubajchel@gmail.com>
2016-04-10 21:42:45 +0200

    Update MenuFrame.java

--- src/projekt/MenuFrame.java
+++ src/projekt/MenuFrame.java
@@ -7,9 +7,18 @@ import javax.swing.*;
 /**
  * Created by Kuba on 2016-04-04.
  */
-
+ 
+/**
+ * Klasa MenuFrame dziedziczy po klasie JFrame. Odpowiada ona za stworzenie ramki menu wewnątrz
+ * której znajdzie się wewnętrzny panel z właściwym menu.
+ */
 public class MenuFrame extends JFrame{
 
+    /**
+     * Konstruktor klasy MenuFrame określający sposób zamknięcia ramki i jej lokalizację początkową
+     * oraz uniemożliwiający zmianę kształtu ramki.
+     */
+     
     public MenuFrame() {
         super("Władek Invaders");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,5 +28,3 @@ public class MenuFrame extends JFrame{
     }
 
 }
-
-
